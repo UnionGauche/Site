f4f9febc176f41b6d4c38daa118d5cc1653b2d7a
UnionGauche <discord.uniongauche@gmail.com>
2017-06-27 15:43:24 +0200

    Enlevé logs superflus

--- js/social.js
+++ js/social.js
@@ -4,13 +4,11 @@ $(document).ready(function() {
             width: "+=3%",
             height: "+=3%"
         }, 'slow');
-        console.log("hovered");
 
     }, function() {
         $(this).animate({
             width: "-=3%",
             height: "-=3%"
         }, 'slow');
-        console.log("de-hovered");
     });
 });
