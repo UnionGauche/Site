77a522098669ddb017bdc05865bdb48fa3dc257a
UnionGauche <discord.uniongauche@gmail.com>
2017-06-27 15:42:30 +0200

    Réduction de la taille de l'effet

--- js/social.js
+++ js/social.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     $('#discord').hover(function() {
         $(this).animate({
-            width: "+=28%",
-            height: "+=28%"
+            width: "+=3%",
+            height: "+=3%"
         }, 'slow');
         console.log("hovered");
 
     }, function() {
         $(this).animate({
-            width: "-=28%",
-            height: "-=28%"
+            width: "-=3%",
+            height: "-=3%"
         }, 'slow');
         console.log("de-hovered");
     });
