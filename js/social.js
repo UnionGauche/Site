878553472a7aaac3eafa69fa8c628d77bd5d4be8
UnionGauche <discord.uniongauche@gmail.com>
2017-06-27 15:33:51 +0200

    Tests de débogage

--- js/social.js
+++ js/social.js
@@ -1,12 +1,14 @@
 $('#discord').hover(function() {
     $(this).animate({
-        width: "28%",
-        height: "28%"
+        width: "+=28%",
+        height: "+=28%"
     }, 'slow');
+    console.log("hovered");
 
 }, function() {
     $(this).animate({
-        width: "-28%",
-        height: "-28%"
+        width: "-=28%",
+        height: "-=28%"
     }, 'slow');
+    console.log("de-hovered");
 });
